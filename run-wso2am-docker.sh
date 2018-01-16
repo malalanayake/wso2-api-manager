179098a7900022d912afd03a5ca65db665fe8b77
Dinuka Malalanayake <dinuka.malalanayake@gmail.com>
2018-01-16 17:39:00 -0600

    Update SH Script

--- run-wso2am-docker.sh
+++ run-wso2am-docker.sh
@@ -1,4 +1,4 @@
 
 #Spinup the Container
 
-docker run -p 9443:9443 -p 9763:9763 -p 8243:8243 -p 8280:8280 -p 10397:10397 -p 7711:7711 sysensor/wso2-api-manager
+docker run -p 9443:9443 -p 9763:9763 -p 8243:8243 -p 8280:8280 -p 10397:10397 -p 7711:7711 sysensor/wso2-api-manager:FHLBDM-POC
